6504942d96aa60c5496d8a19f14f4e03dda29b42
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-13 16:31:42 +0100

    added Procfile

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -114,14 +114,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+
+
 django_heroku.settings(locals())
