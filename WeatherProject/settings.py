c9a9a089cd10a1f8bad069cbf6dbf611b3d48878
abdul mudhir <abdulmudhirss@gmail.com>
2020-04-13 17:31:16 +0100

    staticfile test

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -119,7 +119,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collect static to find static files.
